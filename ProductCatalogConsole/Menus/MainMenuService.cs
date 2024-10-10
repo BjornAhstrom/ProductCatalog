064a41720f02ca0828c9a8ed1744e7286dde89b8
Björn Åhström <bjorn.ahstrom@icloud.com>
2024-10-10 21:39:07 +0200

    Added dependency injections and configured Program class

---
+++ ProductCatalogConsole/Menus/MainMenuService.cs
@@ -0,0 +1,52 @@
+﻿namespace ProductCatalogConsole.Menus;
+
+public class MainMenuService
+{
+    //private static readonly string _productsFile = Path.Combine(AppContext.BaseDirectory, "Products.json");
+    //private static readonly string _categoriesFile = Path.Combine(AppContext.BaseDirectory, "Categories.json");
+
+    //private static ProductInteraction _productInteraction = new ProductInteraction(_productsFile, _categoriesFile);
+    //private static CategoryMenu _categoryMenu = new CategoryMenu(_categoriesFile);
+    public void StartMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("\t[1] Lista alla produkter");
+            Console.WriteLine("\t[2] Lägg till en produkt");
+            Console.WriteLine("\t[3] Ändra en produkt");
+            Console.WriteLine("\t[4] Ta bort en produkt");
+            Console.WriteLine("\t[5] Kategori meny");
+            Console.WriteLine("\t[0] Avsluta");
+
+            Console.Write("\n\tAnge ett menyalternativ: ");
+            int.TryParse(Console.ReadLine(), out int option);
+
+            switch (option)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    //_productInteraction.ListAllProducts();
+                    break;
+                case 2:
+                    //_productInteraction.CreateProductInteraction();
+                    break;
+                case 3:
+                    //_productInteraction.ChangeProductInfo();
+                    break;
+                case 4:
+                    //_productInteraction.DeleteProduct();
+                    break;
+                case 5:
+                    //_categoryMenu.DisplayCategoryMenu();
+                    break;
+                default:
+                    Console.WriteLine("\n\tMenyalternativet finns inte.");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
--- ProductCatalogConsole/Program.cs
+++ ProductCatalogConsole/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductCatalogConsole.Menus;
+using Resources.Services;
+
+class Program
+{
+
+    private static IServiceProvider? _serviceProvider;
+    public static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureService(serviceCollection);
+
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var menuService = _serviceProvider!.GetRequiredService<MainMenuService>();
+        menuService.StartMenu();
+    }
+
+    private static void ConfigureService(IServiceCollection service)
+    {
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        var filePath = Path.Combine(baseDirectory, "products.json");
+
+        service.AddSingleton<IProductService>();
+        service.AddSingleton<IFileService>(new FileService(filePath));
+
+        service.AddSingleton<MainMenuService>();
+    }
+}
